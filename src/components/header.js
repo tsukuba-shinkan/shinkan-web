bab6be273bb31fadc566de2a5c3ee1ee78554a60
Asaga Kosho <asagatto777@gmail.com>
2020-03-21 15:36:26 +0900

    [Update] ヘッダー製作

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,23 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Image from "../components/image"
+import Logo from "../components/logo"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <header>
+    <div>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `black`,
             textDecoration: `none`,
+            fontWeight: `normal`,
           }}
         >
-          {siteTitle}
+          <img src="https://drive.google.com/uc?id=1ReaOXbZz2mHy2CKZ7cQONvot6vp3S02m" alt={`筑波大学`} id={`headerLogo`}/>
+          <span>新歓Web</span>
         </Link>
       </h1>
     </div>
