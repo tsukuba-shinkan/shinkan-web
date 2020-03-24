af93b10e7a6e9bc71985122228d37c9d73110031
Kazumi Inada <hello@nandenjin.com>
2020-03-24 10:21:07 +0900

    [Add] source plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-shinkan-web",
+      options: {
+        fileId:
+          process.env.SRC_FILE_ID ||
+          "1r4c4E7r1PDnaf6Mk6YIS6FAOblLBiX66socla-x8ct4",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
