5b63a7455fdd11d6f4546d88c73c0de824231bef
Asaga Kosho <asagatto777@gmail.com>
2020-03-27 14:35:39 +0900

    [Add] トップページでのポスター一覧表示実装

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,127 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 // import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="ホーム" />
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      sports: allShinkanWebOrg(filter: { activityType: { eq: "体育系" } }) {
+        edges {
+          node {
+            posterImageUrls
+            name
+          }
+        }
+      }
+      culture: allShinkanWebOrg(filter: { activityType: { eq: "文化系" } }) {
+        edges {
+          node {
+            posterImageUrls
+            name
+          }
+        }
+      }
+      art: allShinkanWebOrg(filter: { activityType: { eq: "芸術系" } }) {
+        edges {
+          node {
+            posterImageUrls
+            name
+          }
+        }
+      }
+      others: allShinkanWebOrg(filter: { activityType: { eq: "その他" } }) {
+        edges {
+          node {
+            posterImageUrls
+            name
+          }
+        }
+      }
+    }
+  `)
+  console.log(data)
+  return (
+    <Layout>
+      <SEO title="ホーム" />
+      <div class="categories sports">
+        <h2>体育系</h2>
+        <div>
+          <ul className={"scrollList"}>
+            {data.sports.edges.map(({ node }) => {
+              return (
+                <li className={"scrollItem"}>
+                  <img
+                    src={node.posterImageUrls[0]}
+                    alt={node.name}
+                    loading="lazy"
+                  />
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+
+      <div class="categories culture">
+        <h2>文化系</h2>
+        <div>
+          <ul className={"scrollList"}>
+            {data.culture.edges.map(({ node }) => {
+              return (
+                <li className={"scrollItem"}>
+                  <img
+                    src={node.posterImageUrls[0]}
+                    alt={node.name}
+                    loading="lazy"
+                  />
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+      <div class="categories art">
+        <h2>芸術系</h2>
+        <div>
+          <ul className={"scrollList"}>
+            {data.art.edges.map(({ node }) => {
+              return (
+                <li className={"scrollItem"}>
+                  <img
+                    src={node.posterImageUrls[0]}
+                    alt={node.name}
+                    loading="lazy"
+                  />
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+      <div class="categories others">
+        <h2>その他</h2>
+        <div>
+          <ul className={"scrollList"}>
+            {data.others.edges.map(({ node }) => {
+              return (
+                <li className={"scrollItem"}>
+                  <img
+                    src={node.posterImageUrls[0]}
+                    alt={node.name}
+                    loading="lazy"
+                  />
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
