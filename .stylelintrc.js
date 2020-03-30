37e30dd2a082530ccf15b34f8e13d25a6b935710
Kazumi Inada <hello@nandenjin.com>
2020-03-30 10:10:50 +0900

    [Chore] stylelintの導入

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recess-order"
+  ],
+  rules: {
+    "indentation": 2,
+    "no-descending-specificity": null
+  }
+}
