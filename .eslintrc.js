04d2efcbf03a52acc69ad72bf477f163f86e5165
Kazumi Inada <hello@nandenjin.com>
2020-04-03 12:21:58 +0900

    [Chore] Lintルールをアップデート

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,30 @@
 module.exports = {
+  parserOptions: {
+    "ecmaVersion": 9,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true,
+    }
+  },
+  env: {
+    node: true
+  },
   extends: [
-    "react-app",
+    "eslint:recommended",
+    "plugin:react/recommended",
     "plugin:prettier/recommended",
     "prettier",
     "prettier/react"
   ],
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
   rules: {
+    "react/prop-types": "off",
+    "eqeqeq": "error",
+    "no-console": "error",
     "prettier/prettier": [
       "error",
       {
