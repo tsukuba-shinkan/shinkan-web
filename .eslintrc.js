ef820157ce81c42fb7e788416141b252377343db
Kazumi Inada <hello@nandenjin.com>
2020-04-03 12:33:50 +0900

    [Chore] Lintルール `no-console`をwarnへ降格

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,7 @@ module.exports = {
   rules: {
     "react/prop-types": "off",
     "eqeqeq": "error",
-    "no-console": "error",
+    "no-console": "warn",
     "prettier/prettier": [
       "error",
       {
