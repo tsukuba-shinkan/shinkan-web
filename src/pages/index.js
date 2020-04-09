31808fef6d901551c0a86b3d0c5aa6c63b2317cf
Kazumi Inada <hello@nandenjin.com>
2020-04-09 18:03:25 +0900

    [Add] クエリフィルターを実装

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react"
+import React, { useState, createRef } from "react"
 import { graphql, useStaticQuery, Link, navigate } from "gatsby"
 import "./index.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons"
 
 import Layout from "../components/layout"
 import Fluid200 from "../components/Fluid200"
@@ -8,6 +10,17 @@ import SEO from "../components/seo"
 
 const IndexPage = () => {
   const [lastTouchedItem, setLastTouchedItem] = useState(null)
+  const [queryString, setQueryString] = useState()
+  const filterSearchInputRef = createRef()
+
+  const filterWithQueries = org => {
+    if (queryString && queryString.length > 0) {
+      return org.name.indexOf(queryString) > -1
+    } else {
+      return true
+    }
+  }
+
   const { orgs } = useStaticQuery(graphql`
     {
       orgs: allShinkanWebOrg {
@@ -28,38 +41,60 @@ const IndexPage = () => {
       <SEO title="ホーム" />
 
       <div className="page--index">
-        <ul className="org-list">
-          {orgs.edges.map(({ node: org }) => (
-            <li className="org-list__item" key={org.primaryKey}>
-              <Link to={`/org/${org.primaryKey}`}>
-                <figure className="org-list__item__poster">
-                  <Fluid200 url={org.posterImageUrls[0]} alt="" />
-                  <figcaption>
-                    <h2 className="org-list__item__name">{org.name}</h2>
-                    <p className="org-list__item__activity-introduce">
-                      {org.activityIntroduce.slice(0, 100) + "..."}
-                    </p>
-                  </figcaption>
-                </figure>
-              </Link>
-
-              <div // スマホ用
-                className="org-list__item__sp-caption"
+        <nav className="org-list-filter">
+          <div className="org-list-filter__section org-list-filter__section--search">
+            <span className="org-list-filter__section--search__icon-container">
+              <FontAwesomeIcon
+                icon={
+                  queryString && queryString.length > 0 ? faTimes : faSearch
+                }
                 onClick={() => {
-                  if (lastTouchedItem === org.primaryKey)
-                    navigate(`/org/${org.primaryKey}`)
-                  setLastTouchedItem(org.primaryKey)
+                  setQueryString()
+                  filterSearchInputRef.current.focus()
                 }}
-              >
-                <div className="org-list__item__sp-caption__name">
-                  {org.name}
+              />
+            </span>
+            <input
+              ref={filterSearchInputRef}
+              onChange={e => setQueryString(e.target.value)}
+              value={queryString || ""}
+            />
+          </div>
+        </nav>
+        <ul className="org-list">
+          {orgs.edges
+            .filter(({ node: org }) => filterWithQueries(org))
+            .map(({ node: org }) => (
+              <li className="org-list__item" key={org.primaryKey}>
+                <Link to={`/org/${org.primaryKey}`}>
+                  <figure className="org-list__item__poster">
+                    <Fluid200 url={org.posterImageUrls[0]} alt="" />
+                    <figcaption>
+                      <h2 className="org-list__item__name">{org.name}</h2>
+                      <p className="org-list__item__activity-introduce">
+                        {org.activityIntroduce.slice(0, 100) + "..."}
+                      </p>
+                    </figcaption>
+                  </figure>
+                </Link>
+
+                <div // スマホ用
+                  className="org-list__item__sp-caption"
+                  onClick={() => {
+                    if (lastTouchedItem === org.primaryKey)
+                      navigate(`/org/${org.primaryKey}`)
+                    setLastTouchedItem(org.primaryKey)
+                  }}
+                >
+                  <div className="org-list__item__sp-caption__name">
+                    {org.name}
+                  </div>
+                  <div className="org-list__item__sp-caption__activity-introduce">
+                    {org.activityIntroduce.slice(0, 100) + "..."}
+                  </div>
                 </div>
-                <div className="org-list__item__sp-caption__activity-introduce">
-                  {org.activityIntroduce.slice(0, 100) + "..."}
-                </div>
-              </div>
-            </li>
-          ))}
+              </li>
+            ))}
         </ul>
       </div>
     </Layout>
